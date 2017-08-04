b088b772bfde8ab17ba7403a63f4bf3e850cf656
Eric Zhou <ericfzhou@berkeley.edu>
2017-08-03 23:31:31 -0700

    try binding to port

--- src/main/java/me/ee/bot/Bot.java
+++ src/main/java/me/ee/bot/Bot.java
@@ -1,5 +1,7 @@
 package me.ee.bot;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
@@ -9,8 +11,11 @@ import net.dv8tion.jda.core.exceptions.RateLimitedException;
 public class Bot {
   private static final String TOKEN = "MjEwOTAzNTM0MzQ0ODYzNzQ2.DGVPtQ.7vHFhTtaqGSpfnIUpjOddOXhY3o";
   public static void main(String[] args)
-      throws LoginException, RateLimitedException, InterruptedException {
+      throws LoginException, RateLimitedException, InterruptedException, IOException {
     JDA jda = new JDABuilder(AccountType.BOT).setToken(TOKEN).buildBlocking();
     jda.addEventListener(new MessageListener());
+
+    ServerSocket s = new ServerSocket(Integer.parseInt(System.getenv("PORT")));
+    System.out.println("Booted server.  " + Integer.parseInt(System.getenv("PORT")));
   }
 }
